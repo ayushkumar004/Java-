8a8592050f3d05d16e0047b97efa5245b127ac1f
ayushkumar004 <153125113+ayushkumar004@users.noreply.github.com>
2026-07-27 10:26:32 +0530

    Add EvenOdd program in EvenOdd.java
    Implement EvenOdd program to check if a number is even or odd.

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int a= sc.nextInt();
+        if(a%2==0){
+            System.out.println("even");
+        }
+        else System.out.println("odd");
+        
+    }
+}
+//finding which number is even and which is odd
