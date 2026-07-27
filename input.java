982c7c2f740c3da4bb61067932d59eb9ea348a55
ayushkumar004 <153125113+ayushkumar004@users.noreply.github.com>
2026-07-27 10:23:46 +0530

    Create input.java

---
+++ input.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;//we have to import Scanner to take inut
+
+public class Main{
+    public static void main(String []args){
+        System.out.println("fuck you");
+        System.out.println(200);
+        System.out.println(25000+"ayush");
+        Scanner sc=new Scanner(System.in);//use to take input
+        int age=sc.nextInt();
+        System.out.println(age);
+    }
+}
