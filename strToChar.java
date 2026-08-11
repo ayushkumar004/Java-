b9e1f447ab34106b86f80f87ca638e7c9cac82c5
ayushkumar004 <153125113+ayushkumar004@users.noreply.github.com>
2026-08-11 17:34:14 +0530

    Create strToChar.java

---
+++ strToChar.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String s = "hello";
+
+        char[] arr = s.toCharArray();
+
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
